0e98504d4070054c4a9c44aebe403d69f4199604
Aditya1or0 <adityapandit264@gmail.com>
2024-12-17 23:36:07 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 await connectDB();
 
 app.use("/api/user", userRouter);
